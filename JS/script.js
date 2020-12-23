da55d93ca552c840a9b60d597165e3e5a03e1602
Faijan-Momin <faijanmomin99@gmail.com>
2020-12-23 17:20:04 +0530

    first commit

---
+++ JS/script.js
@@ -0,0 +1,75 @@
+// Home Page - SlideShow Images
+
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+})
+
+
+
+// Adding Guest button
+
+function increment() {
+  document.getElementById('no-of-guest').stepUp();
+}
+
+// Minus Guest Button
+function decrement() {
+  document.getElementById('no-of-guest').stepDown();
+}
+
+function openNav() {
+  document.getElementById("mobile-Nav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mobile-Nav").style.height = "0%";
+}
+     
+
+
+// Mobile Slideshow
+
+  var slideIndex = 0;
+  showSlides();
+
+  function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 8000); // Change image every 2 seconds
+}
